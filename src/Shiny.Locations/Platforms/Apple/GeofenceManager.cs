00546b3c731c306f431d26118fa50a8316341e3c
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2025-05-16 14:02:44 +0200

    don't destroy monitor on stopping monitoring only one geofence - case where the user just re-creates the geofence by editing the radius - maybe a better solution could be integrated

--- src/Shiny.Locations/Platforms/Apple/GeofenceManager.cs
+++ src/Shiny.Locations/Platforms/Apple/GeofenceManager.cs
@@ -53,7 +53,6 @@ public class GeofenceManager(
 
     
     public IList<GeofenceRegion> GetMonitorRegions() => repository.GetList<GeofenceRegion>();
-
     
     public async Task StartMonitoring(GeofenceRegion region)
     {
@@ -70,9 +69,6 @@ public class GeofenceManager(
         repository.Remove<GeofenceRegion>(identifier);
         var mon = await this.GetMonitor();
         mon.RemoveCondition(identifier);
-        
-        if (repository.GetList<GeofenceRegion>().Count == 0)
-            this.DestroyMonitor();
     }
     
 
