13ac7648d78af04ffb929c9e08470812d10db5ee
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2025-06-11 14:56:16 +0200

    improve log

--- src/Shiny.Core/Hosting/Host.cs
+++ src/Shiny.Core/Hosting/Host.cs
@@ -29,7 +29,7 @@ public class Host : IHost
     public static ILoggerFactory LoggingFactory => Current.Logging;
     public static T? GetService<T>() => ServiceProvider.GetService<T>();
 
-#if APPLE
+#if IOS || APPLE
     public static IosPlatform Platform => ServiceProvider.GetRequiredService<IosPlatform>();
     public static IosLifecycleExecutor Lifecycle => ServiceProvider.GetRequiredService<IosLifecycleExecutor>();
 #elif ANDROID
