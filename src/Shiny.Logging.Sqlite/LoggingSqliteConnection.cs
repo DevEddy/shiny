944847fe76836d22fd5d74d3552faf8de952778d
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2025-07-31 10:05:57 +0200

    add the possibility to get the logs and format them

--- src/Shiny.Logging.Sqlite/LoggingSqliteConnection.cs
+++ src/Shiny.Logging.Sqlite/LoggingSqliteConnection.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
@@ -12,14 +14,14 @@ public class LogStore
     [AutoIncrement]
     [PrimaryKey]
     public int Id { get; set; }
-    
     public string Message { get; set; }
     public int EventId { get; set; }
     // public string? Parameters { get; set; }
     public LogLevel LogLevel { get; set; }
     public DateTime TimestampUtc { get; set; }
+    public override string ToString()
+        => $"[{LogLevel}] [{TimestampUtc.ToLocalTime().ToString(CultureInfo.InvariantCulture)}] {Message}";
 }
-
 
 public class LoggingSqliteConnection : SQLiteAsyncConnection
 {
@@ -45,5 +47,17 @@ public class LoggingSqliteConnection : SQLiteAsyncConnection
             return this.Logs.DeleteAsync(expression);
         
         return this.DeleteAllAsync<LogStore>();
+    }
+    
+    public Task<List<LogStore>> GetLogs(Expression<Func<LogStore, bool>>? expression = null)
+    {
+        var query = Logs;
+        if (expression is not null)
+            query = query.Where(expression);
+
+        return query
+            .OrderByDescending(x => x.TimestampUtc)
+            .ToListAsync();
+
     }
 }
